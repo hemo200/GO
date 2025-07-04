9bd7445bbdcf82ad74e2f6654de46df77157587f
Ebrahim Hasan <ebrahim@hemotips.tech>
2025-07-05 01:59:28 +0300

    Adding base templates

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -11,15 +11,20 @@ import (
 // This is the home page
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
+
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/pages/home.tmpl",
+	}
 	// This section will add the comment of the section that will check if the template is accessable or not
-	ts, err := template.ParseFiles("./ui/html/pages/home.tmpl")
+	ts, err := template.ParseFiles(files...)
 
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal  Server Error", http.StatusInternalServerError)
 		return
 	}
-	err = ts.Execute(w, nil)
+	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server  Error", http.StatusInternalServerError)
