32a73f982d4e5b4858fed6332d8920816f0bfb88
Ebrahim Hasan <ebrahim@hemotips.tech>
2025-07-05 02:19:05 +0300

    Adding the static content handler

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,10 +8,12 @@ import (
 // this is the main branch
 func main() {
 	mux := http.NewServeMux()
+	fileserver := http.FileServer(http.Dir("./ui/static/"))
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileserver))
 
 	//the port to be changes to 5000
 	log.Print("starting server on :5000")
