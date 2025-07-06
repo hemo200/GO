88c41f94a687a2f903b49454d247108198f98ade
Ebrahim Hasan <ebrahim@hemotips.tech>
2025-07-07 02:51:52 +0300

    modifying dockerfile

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -9,7 +9,7 @@ import (
 // this is the main branch
 func main() {
 	// this variable is to define the  port of the application
-	addr := flag.String("addr", ":5000", "HTTP network adress")
+	addr := flag.String("addr", ":4000", "HTTP network adress")
 	mux := http.NewServeMux()
 	fileserver := http.FileServer(http.Dir("./ui/static/"))
 	mux.HandleFunc("GET /{$}", home)
@@ -18,7 +18,6 @@ func main() {
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileserver))
 
-	//the port to be changes to 5000
 	log.Printf("starting server on %s", *addr)
 	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
