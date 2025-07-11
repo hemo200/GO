553b8df1cdd72fcec3a37eae2fae5d01494c7934
Ebrahim Hasan <ebrahim@hemotips.tech>
2025-07-11 17:29:38 +0300

    Adding strucutred logging and additional comments

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,24 +2,38 @@ package main
 
 import (
 	"flag"
-	"log"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
 // this is the main branch
 func main() {
-	// this variable is to define the  port of the application
+	// this variable is to define the default port of the application
 	addr := flag.String("addr", ":4000", "HTTP network address")
-	flag.Parse()
+	flag.Parse() //To read values from runtime
+
+	//Creation of a logger
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	//A new HTTP handler
 	mux := http.NewServeMux()
+	//file server handler
 	fileserver := http.FileServer(http.Dir("./ui/static/"))
+
+	//To handle HTTP requests
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileserver))
 
-	log.Printf("starting server on %s", *addr)
+	// To display all application info logs
+	logger.Info("Starting server", "addr", *addr)
+
 	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+
+	//to create error logs when something goes wrong
+	logger.Error(err.Error())
+	os.Exit(1)
 }
