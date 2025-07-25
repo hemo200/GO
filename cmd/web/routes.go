c4a1135ab3c7d63cfb10d9c161c639397f32509a
Ebrahim Hasan <ebrahim@hemotips.tech>
2025-07-25 05:18:11 +0300

    Keeping the repo up to date

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func (app *application) routes() *http.ServeMux {
 	mux := http.NewServeMux()
-	//fileserver := http.FileServer((http.Dir("./ui/static")))
+	fileserver := http.FileServer((http.Dir("./ui/static")))
 	mux.HandleFunc("GET /{$}", app.home)
 	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
 	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
