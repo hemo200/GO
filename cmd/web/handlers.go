aa0856a85ec70c9a2f288d2c4c04e046f490fdee
Ebrahim Hasan <ebrahim@hemotips.tech>
2025-08-09 14:34:46 +0300

    Test

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -34,6 +34,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.PathValue("id"))
+
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
 		return
